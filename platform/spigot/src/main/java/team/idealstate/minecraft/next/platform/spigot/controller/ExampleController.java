4cb6bd5b1c36e47701f8d4df7c0f8b4e1ba56cf5
ketikai <ketikai@idealstate.team>
2025-05-07 16:55:46 +0800

    feat(spigot): 添加示例命令组件的内容
    [skip ci]
    
    Signed-off-by: ketikai <ketikai@idealstate.team>

--- platform/spigot/src/main/java/team/idealstate/minecraft/next/platform/spigot/controller/ExampleController.java
+++ platform/spigot/src/main/java/team/idealstate/minecraft/next/platform/spigot/controller/ExampleController.java
@@ -18,17 +18,24 @@ package team.idealstate.minecraft.next.platform.spigot.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import team.idealstate.minecraft.next.common.banner.Banner;
 import team.idealstate.minecraft.next.common.command.Command;
 import team.idealstate.minecraft.next.common.command.CommandContext;
 import team.idealstate.minecraft.next.common.command.CommandResult;
 import team.idealstate.minecraft.next.common.command.annotation.CommandArgument;
+import team.idealstate.minecraft.next.common.command.annotation.CommandArgument.ConverterResult;
 import team.idealstate.minecraft.next.common.command.annotation.CommandHandler;
+import team.idealstate.minecraft.next.common.command.exception.CommandArgumentConversionException;
 import team.idealstate.minecraft.next.common.context.ContextHolder;
 import team.idealstate.minecraft.next.common.context.annotation.component.Controller;
 import team.idealstate.minecraft.next.common.context.annotation.feature.Environment;
 import team.idealstate.minecraft.next.common.context.aware.ContextHolderAware;
 import team.idealstate.minecraft.next.common.logging.Log;
+import team.idealstate.minecraft.next.common.string.StringUtils;
 import team.idealstate.minecraft.next.common.validate.annotation.NotNull;
 import team.idealstate.minecraft.next.platform.spigot.api.placeholder.Placeholder;
 
@@ -43,13 +50,47 @@ public final class ExampleController implements ContextHolderAware, Command, Pla
     }
 
     @CommandHandler("show {message}")
-    public CommandResult show(@CommandArgument(completerMethod = "messages") String message) {
+    public CommandResult show(@CommandArgument(completer = "completeMessage") String message) {
         Log.info(String.format("show(String): %s", message));
         return CommandResult.success(message);
     }
 
-    public List<String> messages(CommandContext context, String argument) {
+    @CommandHandler("show {target} {message}")
+    public CommandResult show(
+            @CommandArgument(value = "target", completer = "completePlayer", converter = "convertToPlayer") Player player,
+            @CommandArgument(completer = "completeMessage") String message
+    ) {
+        Log.info(String.format("show(Player, String): %s, %s", player, message));
+        player.sendMessage(message);
+        return CommandResult.success();
+    }
+
+    @NotNull
+    public List<String> completePlayer(@NotNull CommandContext context, @NotNull String argument) {
+        String lowerCase = argument.toLowerCase();
+        return Bukkit.getOnlinePlayers().stream()
+                .map(Player::getName)
+                .map(String::toLowerCase)
+                .filter(s -> s.startsWith(lowerCase))
+                .collect(Collectors.toList());
+    }
+
+    @NotNull
+    public List<String> completeMessage(@NotNull CommandContext context, @NotNull String argument) {
         return Arrays.asList("minecraft-next", "hello-next");
+    }
+
+    @NotNull
+    public ConverterResult<Player> convertToPlayer(@NotNull CommandContext context, @NotNull String argument, boolean onConversion) throws CommandArgumentConversionException {
+        Player player = Bukkit.getPlayer(argument);
+        boolean canBeConvert = player != null && player.isOnline();
+        if (!onConversion) {
+            return canBeConvert ? ConverterResult.success() : ConverterResult.failure();
+        }
+        if (!canBeConvert) {
+            throw new CommandArgumentConversionException(String.format("The argument '%s' cannot be convert to player.", argument));
+        }
+        return ConverterResult.success(player);
     }
 
     @CommandHandler
